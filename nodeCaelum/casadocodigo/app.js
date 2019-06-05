e9a764c44a5acee723ed470c7807797434560076
Tas <tas.g.maia@gmail.com>
2019-06-04 22:32:32 -0300

    exercicio 1.12

--- nodeCaelum/casadocodigo/app.js
+++ nodeCaelum/casadocodigo/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 app.get('/produtos', function (req, res) {
     console.log('Recebeu requisição');
-    res.send('<h1>Listagem de produtos</h1>');
+    res.render('produtos/lista');
 });
 const server = app.listen(3000, function(){
     console.log('Servidor rodando em http://localhost:3000');
