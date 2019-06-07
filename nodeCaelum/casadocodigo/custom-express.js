ae00b08d7d748e892be9ec59ea2c300260b00c0d
Tas <tas.g.maia@gmail.com>
2019-06-06 22:06:22 -0300

    app update

--- nodeCaelum/casadocodigo/custom-express.js
+++ nodeCaelum/casadocodigo/custom-express.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const load = require('express-load');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 module.exports = function(){
     const app = express();
     app.use(express.static('./public'));
     app.set('view engine', 'ejs');
     app.use(bodyParser.urlencoded());
     app.use(bodyParser.json());
+    app.use(expressValidator());
     load('routes')
         .then('infra')
         .into(app);
--- nodeCaelum/casadocodigo/routes/produtos.js
+++ nodeCaelum/casadocodigo/routes/produtos.js
@@ -21,6 +21,21 @@ module.exports = function (app){
 
     app.post('/produtos', function(req, res){
         const livro = req.body;
+        req.assert('titulo', 'Titulo deve ser preenchido').notEmpty();
+        req.assert('preco', 'Preco deve ser um numero').isFloat();
+        const errors = req.validationErrors();
+        if (errors){
+            console.log('há erros de validação!');
+            res.format({
+                html: function (){
+                    res.status(400).render('produtos/form', { validationErrors: errors });
+                },
+                json: function (){
+                    res.status(400).send(errors);
+                }
+            });
+            return;
+        } 
         const connection = app.infra.connectionFactory();
         const produtoDao = new app.infra.ProdutoDao(connection);
         produtoDao.salva(livro, function(exception, results){
