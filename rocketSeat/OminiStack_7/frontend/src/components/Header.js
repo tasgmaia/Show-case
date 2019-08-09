afd12531d2ff3b6f7a393b2295750d6695a3bf41
Tas <tas.g.maia@gmail.com>
2019-08-09 19:10:50 -0300

    create header

---
+++ rocketSeat/OminiStack_7/frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+      <header id="main-header">
+          <div className="header-content">
+              <img src="" alt="instaRocket"/>
+              <img src="" alt="Enviar publicação"/>
+          </div>
+      </header>
+  );
+}
