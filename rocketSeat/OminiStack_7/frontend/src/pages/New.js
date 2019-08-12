d04e341cbfab6f5b32637c34a682ce3ed4ca733e
Tas <tas.g.maia@gmail.com>
2019-08-12 19:52:35 -0300

    start feed

--- rocketSeat/OminiStack_7/frontend/src/pages/New.js
+++ rocketSeat/OminiStack_7/frontend/src/pages/New.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './new.css';
+
 class New extends Component {
     render(){
         return(
--- rocketSeat/OminiStack_7/frontend/src/pages/feed.js
+++ rocketSeat/OminiStack_7/frontend/src/pages/feed.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
+import './feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
     render(){
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Jonatas Guerci Maia</span>
+                            <span className="place">Rio de Janeiro</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                </article>
+            </section>
         );
     }
 }
