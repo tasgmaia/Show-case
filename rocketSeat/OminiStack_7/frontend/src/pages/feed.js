c9d31bf75580cf7512194f8cb5d6ff70fb500df2
Tas <tas.g.maia@gmail.com>
2019-08-13 19:26:13 -0300

    pretify

--- rocketSeat/OminiStack_7/frontend/src/pages/feed.js
+++ rocketSeat/OminiStack_7/frontend/src/pages/feed.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './feed.css';
+import "./feed.css";
 
-import more from '../assets/more.svg';
-import like from '../assets/like.svg';
-import comment from '../assets/comment.svg';
-import send from '../assets/send.svg';
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 
 class Feed extends Component {
-    render(){
-        return(
-            <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Jonatas Guerci Maia</span>
-                            <span className="place">Rio de Janeiro</span>
-                        </div>
-                        <img src={more} alt="Mais"/>
-                    </header>
-                </article>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Jonatas Guerci Maia</span>
+              <span className="place">Rio de Janeiro</span>
+            </div>
+            <img src={more} alt="Mais" />
+          </header>
+        </article>
+      </section>
+    );
+  }
 }
 
 export default Feed;
